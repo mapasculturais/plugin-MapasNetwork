475f7dada76e4ec9084bf50f4ab48485538ac24f
diogo <you@example.com>
2021-12-16 23:00:08 -0300

    Inserçao de container para manipular o switch e alteraçao da posiçao da palavra sincronizar

--- layouts/parts/network-node/entity-sync-switch.php
+++ layouts/parts/network-node/entity-sync-switch.php
@@ -1,9 +1,9 @@
 <div class="container-switch"> 
+    Sincronizar
     <label>
         <span class="switch" ng-controller="MapasNetworkController" style="vertical-align: middle;">
             <input type="checkbox" ng-model="entityShouldSync" ng-change="toggleSync()" />
             <span class="slider"></span>
         </span>
-        Sincronizar
     </label>
 </div> 
